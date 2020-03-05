f0a33e56c1e64acb7538cca76a3657df927b7d3f
Maxime Druart <maxime41010.druart@gmail.com>
2020-03-05 08:22:14 +0100

    working on transitions

--- scripts/wiggly.js
+++ scripts/wiggly.js
@@ -8,16 +8,22 @@ const app = new Application({
   width: window.innerWidth,
   height: window.innerHeight,
   antialias: true,
-  transparent: true,
+  //   transparent: true,
+  backgroundColor: 0x131313,
   resolution: 1
 })
 
 document.body.appendChild(app.view)
+loader.add("./images/img.jpg").load(setup)
+
+const map = (n, start1, stop1, start2, stop2) => ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2
+const constrain = (n, low, high) => Math.max(Math.min(n, high), low)
+noise.seed(Math.random())
 
 let yOffset = 0
 const c = {
-  noiseStrength: 0.15,
-  vertexCount: 45,
+  noiseStrength: 0.07,
+  vertexCount: 30,
   yOffsetIncrement: 0.01,
   size: {
     value: 230,
@@ -25,27 +31,6 @@ const c = {
     variation: 15
   }
 }
-
-const s = {
-  noiseStrength: 0.1,
-  vertexCount: 60,
-  yOffsetIncrement: 0.01,
-  size: {
-    w: 230,
-    h: 230,
-    variation: 15
-  }
-}
-
-const map = function(n, start1, stop1, start2, stop2) {
-  return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2
-}
-
-const constrain = (n, low, high) => Math.max(Math.min(n, high), low)
-
-let bg, mask
-loader.add("./images/img.jpg").load(setup)
-let pn = new Perlin("planting my seed owo")
 
 const listenToMouseEvents = () => {
   window.addEventListener("mousemove", e => {
@@ -63,7 +48,7 @@ function setup() {
   bg.anchor.set(0.5, 0.5)
   bg.x = window.innerWidth / 2
   bg.y = window.innerHeight / 2
-  app.stage.addChild(bg)
+  //   app.stage.addChild(bg)
 
   mask = new PIXI.Graphics()
   app.stage.addChild(mask)
@@ -77,24 +62,17 @@ function setup() {
   app.ticker.speed = 0.1
   app.ticker.add(() => {
     yOffset += c.yOffsetIncrement
-    c.size.value += pn.noise(0, 0, yOffset) / 2 - 0.25
-    c.size.value = _.clamp(c.size.value, c.size.baseValue - c.size.variation, c.size.baseValue + c.size.variation)
+    c.size.value += map(noise.simplex3(0, 0, yOffset), -1, 1, -0.25, 0.25)
+    c.size.value = constrain(c.size.value, c.size.baseValue - c.size.variation, c.size.baseValue + c.size.variation)
 
     let points = []
     const getCircle = () => {
       for (let i = 0; i < c.vertexCount; i++) {
         let angleRad = ((i / c.vertexCount) * 360 * Math.PI) / 180
         let angleDeg = (i / c.vertexCount) * 360
-        // console.log(pn.noise(angleDeg, yOffset, 0), Math.cos(angleRad))
-        let x =
-          Math.cos(angleRad) *
-          c.size.value *
-          map(pn.noise(angleDeg, yOffset, 0), 0, 1, 1 - c.noiseStrength, 1 + c.noiseStrength)
-
-        let y =
-          Math.sin(angleRad) *
-          c.size.value *
-          map(pn.noise(angleDeg, yOffset, 0), 0, 1, 1 - c.noiseStrength, 1 + c.noiseStrength)
+        let noiseVal = map(noise.simplex2(angleDeg, yOffset), -1, 1, 1 - c.noiseStrength, 1 + c.noiseStrength)
+        let x = Math.cos(angleRad) * c.size.value * noiseVal
+        let y = Math.sin(angleRad) * c.size.value * noiseVal
 
         points.push({ x, y })
       }
@@ -103,7 +81,8 @@ function setup() {
     getCircle()
 
     mask.clear()
-    mask.beginFill(0)
+    mask.lineStyle(6, 0xffffff)
+    // mask.beginFill(0)
 
     // https://stackoverflow.com/questions/7054272/how-to-draw-smooth-curve-through-n-points-using-javascript-html5-canvas
 
@@ -122,9 +101,8 @@ function setup() {
     yc = (points[0].y + points[1].y) / 2
     mask.quadraticCurveTo(points[0].x, points[0].y, xc, yc)
 
-    mask.closePath()
     mask.endFill()
-    bg.mask = mask
+    // bg.mask = mask
   })
 
   listenToMouseEvents()
